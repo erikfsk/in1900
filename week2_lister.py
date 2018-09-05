50bc5e62fb77be44d406a8ea9c13045e158e328e
Unknown <erikfsk@student.matnat.uio.no>
2018-09-05 14:08:50 +0200

    tips for week2

--- week2_lister.py
+++ week2_lister.py
@@ -1,5 +1,5 @@
-t = []
-y = []
+# t = [i for i in range(11)]
+# y = [i*i for i in t]
 
 for i in range(11):
     t.append(i)
@@ -8,12 +8,8 @@ for i in range(11):
 ty1 = [t,y]
 ty2 = [[i,j] for i,j in zip(t,y)]
 
-for tmp in range(len(ty1[0])):
-    print ty1[0][tmp],ty1[1][tmp] 
 
-
-
-print "t",t
-print "y",y
-print "ty1",ty1
-print "ty2",ty2
+print( "t",t)
+print( "y",y)
+print( "ty1",ty1)
+print( "ty2",ty2)
